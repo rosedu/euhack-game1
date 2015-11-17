3a363e7b22c6485c4b799d824efb6250d230dfb4
Alex Morega <alex@grep.ro>
2015-11-17 11:25:55 +0100

    move ball towards mouse

--- game.js
+++ game.js
@@ -1,13 +1,26 @@
 var game = document.querySelector('#game');
-var n = 20;
+var world = {
+    ball: {x: 0, y: 0},
+    mouse: {x: 0, y: 0}
+};
+var speed = 2;
 
-function draw() {
-    n = move(n);
-    document.querySelector('#ball').setAttribute('cx', n);
+function moveBall(ball, mouse) {
+    var dx = mouse.x - ball.x;
+    var dy = mouse.y - ball.y;
+    var distance = Math.sqrt(dx*dx + dy*dy);
+    if(distance < 1) {
+        return ball;
+    }
+    var f = speed / distance;
+    return {x: ball.x + dx * f, y: ball.y + dy * f};
 }
 
-function move(x) {
-    return x + 1;
+function draw() {
+    world.ball = moveBall(world.ball, world.mouse);
+    var circle = document.querySelector('#ball');
+    circle.setAttribute('cx', world.ball.x);
+    circle.setAttribute('cy', world.ball.y);
 }
 
 function gameLoop() {
@@ -15,4 +28,9 @@ function gameLoop() {
     requestAnimationFrame(gameLoop);
 }
 
-window.addEventListener('load', gameLoop);
+window.addEventListener('load', function() {
+    game.addEventListener('mousemove', function(e) {
+        world.mouse = {x: e.x, y: e.y};
+    });
+    gameLoop();
+});
