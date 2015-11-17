053e53bd221db2d7cd2e3f2c1b3f3c8ae66fc276
Alex Morega <alex@grep.ro>
2015-11-17 11:01:44 +0100

    extract game loop

--- game.js
+++ game.js
@@ -1,17 +1,18 @@
 var game = document.querySelector('#game');
-
 var n = 20;
-move(n);
-requestAnimationFrame(draw);
 
 function draw() {
     n = move(n);
-    if(n <= 100) {
-        requestAnimationFrame(draw);
-    }
     document.querySelector('#ball').setAttribute('cx', n);
 }
 
 function move(x) {
     return x + 1;
 }
+
+function gameLoop() {
+    draw();
+    requestAnimationFrame(gameLoop);
+}
+
+window.addEventListener('load', gameLoop);
