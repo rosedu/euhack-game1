2e54b402e12b8ad643dc8cf4d0dd8e06273565bb
Alex Morega <alex@grep.ro>
2015-11-17 16:15:50 +0100

    move ball with keys

--- game.js
+++ game.js
@@ -1,36 +1,40 @@
-var game = document.querySelector('#game');
-var world = {
-    ball: {x: 0, y: 0},
-    mouse: {x: 0, y: 0}
-};
-var speed = 2;
+var keyboard = {};
+var x = 10;
 
-function moveBall(ball, mouse) {
-    var dx = mouse.x - ball.x;
-    var dy = mouse.y - ball.y;
-    var distance = Math.sqrt(dx*dx + dy*dy);
-    if(distance < 1) {
-        return ball;
+function moveLeft() {
+    x = x - 3;
+}
+
+function moveRight() {
+    x = x + 3;
+}
+
+function inputs() {
+    if(keyboard.A) {
+        moveLeft();
     }
-    var f = speed / distance;
-    return {x: ball.x + dx * f, y: ball.y + dy * f};
+    if(keyboard.D) {
+        moveRight();
+    }
 }
 
 function draw() {
-    world.ball = moveBall(world.ball, world.mouse);
     var circle = document.querySelector('#ball');
-    circle.setAttribute('cx', world.ball.x);
-    circle.setAttribute('cy', world.ball.y);
+    circle.setAttribute('cx', x);
 }
 
 function gameLoop() {
+    inputs();
     draw();
     requestAnimationFrame(gameLoop);
 }
 
 window.addEventListener('load', function() {
-    game.addEventListener('mousemove', function(e) {
-        world.mouse = {x: e.x, y: e.y};
+    window.addEventListener('keydown', function(e) {
+        keyboard[String.fromCharCode(e.keyCode)] = true;
+    });
+    window.addEventListener('keyup', function(e) {
+        keyboard[String.fromCharCode(e.keyCode)] = false;
     });
     gameLoop();
 });
