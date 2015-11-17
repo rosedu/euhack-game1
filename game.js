03a3c12fc5f756ac1ab8dc5fa48a9d697be4129c
Alex Morega <alex@grep.ro>
2015-11-17 17:04:43 +0100

    collision with ground

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 var keyboard = {};
-var x = 10;
+var x = 20;
+var y = 20;
 
 function moveLeft() {
     x = x - 3;
@@ -18,13 +19,26 @@ function inputs() {
     }
 }
 
+function physics() {
+    y += .1;
+    var ball = {x: x - 10, y: y - 10, h: 20, w: 20};
+    world.forEach(function(obj) {
+        if(ball.y + ball.h > obj.y) {
+            ball.y = obj.y - ball.h;
+        }
+    });
+    y = ball.y + 10;
+}
+
 function draw() {
     var circle = document.querySelector('#ball');
     circle.setAttribute('cx', x);
+    circle.setAttribute('cy', y);
 }
 
 function gameLoop() {
     inputs();
+    physics();
     draw();
     requestAnimationFrame(gameLoop);
 }
@@ -33,15 +47,15 @@ var world;
 window.addEventListener('load', function() {
     world = [];
     [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].forEach(function(n) {
-        var obj = {id: 'block'+n, x: n*32, y: 48};
+        var obj = {id: 'block'+n, x: n*32, y: 48, w: 32, h :32};
         world.push(obj);
         node = document.createElementNS('http://www.w3.org/2000/svg', 'image');
         node.setAttributeNS('http://www.w3.org/1999/xlink', 'href', 'ground.png');
         node.setAttribute('id', obj.id);
         node.setAttribute('x', obj.x);
         node.setAttribute('y', obj.y);
-        node.setAttribute('width', 32);
-        node.setAttribute('height', 32);
+        node.setAttribute('width', obj.w);
+        node.setAttribute('height', obj.h);
         document.querySelector('#game').appendChild(node);
     });
 
